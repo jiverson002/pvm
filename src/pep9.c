jiverson002/pvm

commit 5c4191f5d1797ca9f5267fd144dfdfbc39e43a18

pvm: Added cpu abstraction and requirement for examine()
The cpu abstraction steps from the new requirement for vms to implement an
examine() function. The purpose of examine() is to allow an observer of
a vm, i.e., the program using a pep vm, to unobtrusively examine its cpu
state and its memory. Unobtrusive here means that examine() can have no
side-effects on the state of the vm.

src/pep9.c
@@ -482,6 +482,7 @@ static void (*i2f[256])(void) = {
 /******************************************************************************/
 /* VM API */
 /******************************************************************************/
+#include <stdarg.h>
 #include <string.h>
 
 static int burn(/*@unique@*/ void const * const os, unsigned const os_len, unsigned const addr) {
@@ -609,6 +610,7 @@ static int init(void) {
   }
 
   /* setup cpu registers */
+  NZVC = 0x0000;
   A  = 0x0000;
   X  = 0x0000;
   PC = 0x0000;
@@ -621,6 +623,8 @@ static int init(void) {
 
 static int load(/*@unique@*/ void const * const prog, unsigned const prog_len) {
   if (!(bool)prog)
+    return -1;
+  if (0 == prog_len)
     return -1;
   if (prog_len > 0x10000u) /* FIXME create better size constraints */
     return -1;
@@ -657,4 +661,43 @@ static int step(void) {
   return (int)IR;
 }
 
-struct vm const pep9 = { burn, init, load, step };
+static int examine(int const mode, ...) {
+  va_list list;
+
+  va_start(list, mode);
+
+  if (0 != (mode & CPU)) {
+    struct cpu * const cpu = va_arg(list, struct cpu *);
+
+    cpu->nzvc = NZVC;
+    cpu->a = A;
+    cpu->x = X;
+    cpu->pc = PC;
+    cpu->sp = SP;
+    cpu->ir = IR;
+    cpu->opspec = OpSpec;
+  }
+
+  if (0 != (mode & MEM)) {
+    byte * const mem    = va_arg(list, void *);
+    unsigned const addr = va_arg(list, unsigned);
+    unsigned const len  = va_arg(list, unsigned);
+
+    if (addr > 0xffffu) {
+      va_end(list);
+      return -1;
+    }
+    if (len > 0x10000u || addr > 0x10000u - len) {
+      va_end(list);
+      return -1;
+    }
+
+    memcpy(mem, Mem + addr, len);
+  }
+
+  va_end(list);
+
+  return 0;
+}
+
+struct vm const pep9 = { burn, init, load, step, examine };

cmake/pvm.h.in
@@ -5,11 +5,27 @@
 #define PVM_MINOR @PROJECT_VERSION_MINOR@
 #define PVM_PATCH @PROJECT_VERSION_PATCH@
 
+enum {
+  CPU = 1,
+  MEM = 2
+};
+
+struct cpu {
+  int nzvc;
+  int a;
+  int x;
+  int pc;
+  int sp;
+  int ir;
+  int opspec;
+};
+
 struct vm {
   int (*burn)(/*@ unique @*/ void const *, unsigned, unsigned);
   int (*init)(void);
   int (*load)(/*@ unique @*/ void const *, unsigned);
   int (*step)(void);
+  int (*examine)(int, ...);
 };
 
 extern struct vm const pep9;

commit 58188022796ecc0aef6674fba93d5218ada9ce48

pep9: Fixed splint errors introduced by 5c4191f5d1797ca9f5267fd144dfdfbc39e43a18

src/pep9.c
@@ -669,13 +669,13 @@ static int examine(int const mode, ...) {
   if (0 != (mode & CPU)) {
     struct cpu * const cpu = va_arg(list, struct cpu *);
 
-    cpu->nzvc = NZVC;
-    cpu->a = A;
-    cpu->x = X;
-    cpu->pc = PC;
-    cpu->sp = SP;
-    cpu->ir = IR;
-    cpu->opspec = OpSpec;
+    cpu->nzvc = (int)NZVC;
+    cpu->a = (int)A;
+    cpu->x = (int)X;
+    cpu->pc = (int)PC;
+    cpu->sp = (int)SP;
+    cpu->ir = (int)IR;
+    cpu->opspec = (int)OpSpec;
   }
 
   if (0 != (mode & MEM)) {
@@ -692,7 +692,7 @@ static int examine(int const mode, ...) {
       return -1;
     }
 
-    memcpy(mem, Mem + addr, len);
+    memcpy(mem, Mem + addr, (size_t)len);
   }
 
   va_end(list);