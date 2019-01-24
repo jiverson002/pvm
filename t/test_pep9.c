e86691460af3af7abec5f5d631fe2b177f094f66
Jeremy Iverson <jiverson002@csbsju.edu>
2019-01-24 09:36:42 -0600

    test: Added a tests for init() and load()

--- t/test_pep9.c
+++ t/test_pep9.c
@@ -17,8 +17,43 @@ void setup(void)
   ck_assert_int_eq(ret, 0);
 }
 
-START_TEST(test_1)
+START_TEST(t_init_w_default_os)
 {
+  int ret;
+  struct cpu cpu;
+
+  /* Get initial state of cpu. */
+  ret = vm.examine(CPU, &cpu);
+  ck_assert_int_eq(ret, 0);
+
+  /* Check initial state of cpu. */
+  ck_assert_uint_eq(cpu.nzvc, 0x0000);
+  ck_assert_uint_eq(cpu.a, 0x0000);
+  ck_assert_uint_eq(cpu.x, 0x0000);
+  ck_assert_uint_eq(cpu.pc, 0x0000);
+  ck_assert_uint_eq(cpu.sp, 0xfb8f);
+  ck_assert_uint_eq(cpu.ir, 0x0000);
+  ck_assert_uint_eq(cpu.opspec, 0x0000);
+}
+END_TEST
+
+START_TEST(t_load_w_null_prog)
+{
+  int ret;
+
+  /* Call with size of 1, so only the prog pointer will cause error. */
+  ret = vm.load(NULL, 1);
+  ck_assert_int_ne(ret, 0);
+}
+END_TEST
+
+START_TEST(t_load_w_zero_prog_len)
+{
+  int ret;
+
+  /* Call with non-null prog pointer 1, so only the size will cause error. */
+  ret = vm.load((void*)1, 0);
+  ck_assert_int_ne(ret, 0);
 }
 END_TEST
 
@@ -35,7 +70,9 @@ int main(void)
   tcase_add_checked_fixture(tc, setup, NULL);
 
   /* Add tests. */
-  tcase_add_test(tc, test_1);
+  tcase_add_test(tc, t_init_w_default_os);
+  tcase_add_test(tc, t_load_w_null_prog);
+  tcase_add_test(tc, t_load_w_zero_prog_len);
 
   /* Populate suite. */
   suite_add_tcase(s, tc);
